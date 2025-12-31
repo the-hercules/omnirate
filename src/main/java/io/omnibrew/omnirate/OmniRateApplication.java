aa285402d10519941e9748de696c38b55c7b51a9
Aashish Sharma <aashish2535@gmail.com>
2025-12-31 14:03:08 +0530

    initial commit

---
+++ src/main/java/io/omnibrew/omnirate/OmniRateApplication.java
@@ -0,0 +1,13 @@
+package io.omnibrew.omnirate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniRateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmniRateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/omnibrew/omnirate/OmniRateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.omnibrew.omnirate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniRateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
